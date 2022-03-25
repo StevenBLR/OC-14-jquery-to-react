518ced93c2c65102087c7d0a0b663536db467e3e
StevenBLR_PC <s.belair95@gmail.com>
2022-03-25 18:51:47 +0100

    Created createEmployee + Employee page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateEmployee from "./pages/CreateEmployee";
+import Employees from "./pages/Employees";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateEmployee />}></Route>
+          <Route path="/employees" element={<Employees />}></Route>
+        </Routes>
+      </BrowserRouter>
+      ,
+    </div>
+  );
 }
 
 export default App;
