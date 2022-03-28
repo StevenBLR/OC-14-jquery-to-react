cf9bc0ac463ed53ffa9c418c2bd4a9ca681f5e0f
StevenBLR_PC <s.belair95@gmail.com>
2022-03-28 23:46:38 +0200

    Home bt

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,34 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import EmployeesTable from "../components/EmployeesTable";
-import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Employees() {
-  const { register } = useForm();
-
   return (
     <EmployeeStyled>
+      <Link to="/">Home</Link>
       <h1>Current Employees</h1>
-      {
-        //#region Hand made entries
-        /* <form className="employees">
-        <span className="employees__entries" style={entriesStyle}>
-          <select {...register("entries")}>
-            {viewOptions.map((v, i) => (
-              <option
-                value={v}
-                key={`view_${i}`}
-                onChange={() => onEntriesChange}
-              >
-                {v}
-              </option>
-            ))}
-          </select>
-          <p style={{ marginLeft: "15px" }}>Entries</p>
-        </span>
-      </form> */
-        //#endregion
-      }
       <EmployeesTable />
     </EmployeeStyled>
   );
@@ -37,8 +16,12 @@ function Employees() {
 export default Employees;
 
 const EmployeeStyled = styled.div`
+  margin: 25px;
   select {
     width: 15px;
     margin: auto;
+  }
+  h1 {
+    text-align: center;
   }
 `;
